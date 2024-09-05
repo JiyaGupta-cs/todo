1e6a9b41c47207e83ff9ae9e82cb3e9bb82d36de
Jiya Gupta <coderaholic21@gmail.com>
2024-09-05 19:51:58 +0530

    change

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,3 +1,3 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const TODO_PROGRAM_PUBKEY = new PublicKey("GbDLRwmZjTyamfzCbzjB4r8RgUyopWhhCNwtj9aFn4oM");
+export const TODO_PROGRAM_PUBKEY = new PublicKey("FHi9b593PHHVjcrvRBXjAmRd6JsHkWCoLmC8CHdkDUy3");
